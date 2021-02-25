cd3ed506364613518d461ff7be08c4d52f42fd4a
Michael Peter Gudz <michaelp.gudz@gmail.com>
2021-02-25 01:08:53 +0100

    Refactored GetBoard Method

--- Polish-draughts/Program.cs
+++ Polish-draughts/Program.cs
@@ -6,27 +6,42 @@ namespace Polish_draughts
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Pawn[,] array = GetBoard();
+            ShowBoard(array);
+            var makingMove = new Game();
+            Pawn[,] newArray = makingMove.MakeMove(array);
+            Console.WriteLine();
+            ShowBoard(newArray);
+        }
         // Get Board with proper amount and placement of Pawns
         private static Pawn[,] GetBoard()
         {
             Console.WriteLine("Enter Board size: ");
             
             var boardSize = Console.ReadLine();
-            var result = Board.GetInstance(int.Parse(boardSize));
+            int boardSizeInt = int.Parse(boardSize);
+            var result = Board.GetInstance(boardSizeInt);
             var pole = result.GetBoardSize();
             var array = result.GetArray();
             int uBound0 = array.GetUpperBound(0); // Getting upper number of row
             int uBound1 = array.GetUpperBound(1); // Getting upper number of column 
             int amountOfBlackPawns = 0;
             int amountOfWhitePawns = 0;
-            
+
+
+            void PopulateBoard(Pawn[,] array, int boardSize, int pawnsAmount, bool isWhite)
+            {
+                
+            }
             // Putting black pawns on board
             for (int i = 0; i <= uBound0; i++)
             {
                 for (int j = 0; j <= uBound1; j++)
                 {
                     // Amount of pawns for one player equals half of board size * half of board size minus 2 empty rows! 
-                    if (amountOfBlackPawns < (int.Parse(boardSize) - 2) / 2 * (int.Parse(boardSize) / 2)) 
+                    if (amountOfBlackPawns < (boardSizeInt - 2) / 2 * (boardSizeInt / 2)) 
                     {
                         if (((i % 2 == 0) & (j % 2 == 1)) | ((i % 2 == 1) & (j % 2 == 0)))
                         {
@@ -45,7 +60,7 @@ namespace Polish_draughts
                 for (int j = 0 ; j <= uBound1; j++)
                 {
                     // Amount of pawns for one player equals half of board size * half of board size minus 2 empty rows! 
-                    if (amountOfWhitePawns < (int.Parse(boardSize) - 2) / 2 * (int.Parse(boardSize) / 2)) 
+                    if (amountOfWhitePawns < (boardSizeInt - 2) / 2 * (boardSizeInt / 2)) 
                     {
                         if (((i % 2 == 0) & (j % 2 == 1)) | ((i % 2 == 1) & (j % 2 == 0)))
                         {
@@ -57,12 +72,11 @@ namespace Polish_draughts
                     }
                 }
             }
-
             return array;
         }
 
         // Put Board on the screen with Console.WriteLine...
-        private static void SeeBoard(Pawn[,] array)
+        private static void ShowBoard(Pawn[,] array)
         {
             int uBound0 = array.GetUpperBound(0);
             int uBound1 = array.GetUpperBound(1);
@@ -81,17 +95,5 @@ namespace Polish_draughts
             }
         }
         
-        // Main program below
-        static void Main(string[] args)
-        {
-            
-            Pawn[,] array = GetBoard();
-            SeeBoard(array);
-            var makingMove = new Game();
-            Pawn[,] newArray = makingMove.MakeMove(array);
-            Console.WriteLine();
-            SeeBoard(newArray);
-
-        }
     }
 }
